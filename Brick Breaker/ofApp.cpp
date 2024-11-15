7112fb52e5a7806aa87aca45b03e808b306f9b86
Jakub-Wittrock <138022957+Jakub-Wittrock@users.noreply.github.com>
2024-11-14 20:45:30 -0500

    Create ofApp.cpp

---
+++ Brick Breaker/ofApp.cpp
@@ -0,0 +1,107 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+
+}
+//--------------------------------------------------------------
+void ofApp::update() {
+
+}
+
+//--------------------------------------------------------------
+
+void ofApp::draw() {
+}
+
+
+void ofApp::resetGame()
+{
+	
+}
+void ofApp::checkForCollisions()
+{
+	
+}
+void ofApp::setUpBricks()
+{
+	
+}
+void ofApp::setUpColors()
+{
+	
+}
+void ofApp::updateBallPosition()
+{
+	
+}
+
+float* ofApp::getUniformSamples(int n, float min, float max,float *step)
+{
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) 
+{
+	if (key == OF_KEY_LEFT)
+	{
+
+	}
+	if (key == OF_KEY_RIGHT)
+	{
+		
+	}
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+
+}
+
