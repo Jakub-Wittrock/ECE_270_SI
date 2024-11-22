1a82d304f8b0fb08654b32d82c5d192535d2982a
Jakub-Wittrock <138022957+Jakub-Wittrock@users.noreply.github.com>
2024-11-22 10:46:13 -0500

    Create Circle.cpp

---
+++ Non Intersecting Classes Circle/Circle.cpp
@@ -0,0 +1,78 @@
+#include "Circle.h"
+
+Circle::Circle()
+{
+	
+}
+
+Circle::Circle(int x_in, int y_in, int r_in)
+{
+
+}
+
+Circle::Circle(int x_in, int y_in, int r_in, ofColor color_in)
+{
+
+}
+
+Circle::Circle(ofPoint center_in, int r_in)
+{
+
+}
+
+void Circle::set(int x_in, int y_in, int r_in)
+{
+
+}
+
+void Circle::set(int x_in, int y_in, int r_in, ofColor color_in)
+{
+
+}
+
+void Circle::set(ofPoint center_in, int r_in)
+{
+
+}
+
+void  Circle::getRandCircle(int xMin, int xMax, int yMin, int yMax, int rMin, int rMax)
+{
+
+}
+
+void Circle::setColor(ofColor color_in)
+{
+
+}
+
+void Circle::setColor(int r_in, int g_in, int b_in)
+{
+
+}
+
+void Circle::copy(Circle other)
+{
+
+}
+
+void Circle::print(string label)
+{
+
+
+}
+
+void Circle::draw()
+{
+
+}
+
+bool Circle::intersects(Circle other)
+{
+	
+}
+
+
+bool Circle::intersects(int n, Circle* others)
+{
+	
+}
