ce6b4e5d8600601ef30d598391e4043ce1d7592a
Jakub-Wittrock <138022957+Jakub-Wittrock@users.noreply.github.com>
2024-12-06 10:30:05 -0500

    Create Stack.cpp

---
+++ Int Stack/Stack.cpp
@@ -0,0 +1,41 @@
+Stack::Stack()
+{
+
+}
+Stack::Stack(int maxCapacity)
+{
+
+}
+Stack::Stack(int n_in, int* data_in)
+{
+
+}
+void Stack::push(int value)
+{
+
+}
+int Stack::pop()
+{
+	
+}
+int Stack::getData(int* output)
+{
+	
+}
+void Stack::push(int n_in, int* data_in)
+{
+	
+}
+int Stack::getN()
+{
+
+}
+int Stack::getCapacity()
+{
+
+}
+
+void Stack::print(string label)
+{
+
+}
