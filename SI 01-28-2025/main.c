cd38a16d5006658a28a12b3dbd9682b24029148a
Jakub-Wittrock <138022957+Jakub-Wittrock@users.noreply.github.com>
2025-01-28 14:48:20 -0500

    Create main.c

---
+++ SI 01-28-2025/main.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+//-------------------------------------------------------------
+//Function Prototypes
+//-------------------------------------------------------------
+
+int clip(int value, int min, int max);
+int isInRange(int value, int min, int max);
+int getValueInRange(int min, int max);
+
+float getCircleArea(float r);
+float getSquareArea(float sideLength);
+float getTriangleArea(float base, float height);
+
+float getDistance(float x1, float y1, float x2, float y2);
+float checkIntersection(float x1, float y1, float r1, float x2, float y2, float r2);
+
+float map(float value, float inMin, float outMin, float inMax, float outMax);
+float farenheitToCelcius(float f);
+float celsiusToFarenheit(float c);
+
+float parallelResistance(float r1, float r2);
+float seriesResistance(float r1, float r2);
+float getPower(float current, float voltage);
+
+
+
+
+//-------------------------------------------------------------
+// Main
+//-------------------------------------------------------------
+
+int main()
+{
+	//Pointer Practice
+
+	//Function Practice
+
+
+	printf("\n");
+	system("pause");
+}
+
+
+//-------------------------------------------------------------
+//Function Definitions
+//-------------------------------------------------------------
+
+int clip(int value, int min, int max)
+{
+
+}
+int isInRange(int value, int min, int max)
+{
+
+}
+int getValueInRange(int min, int max)
+{
+
+}
+
+float getCircleArea(float r)
+{
+
+}
+float getSquareArea(float sideLength)
+{
+
+}
+float getTriangleArea(float base, float height)
+{
+
+}
+
+float getDistance(float x1, float y1, float x2, float y2)
+{
+
+}
+float checkIntersection(float x1, float y1, float r1, float x2, float y2, float r2)
+{
+
+}
+
+float map(float value, float inMin, float outMin, float inMax, float outMax)
+{
+
+}
+float farenheitToCelcius(float f)
+{
+
+}
+float celsiusToFarenheit(float c)
+{
+
+}
+
+float parallelResistance(float r1, float r2)
+{
+
+}
+float seriesResistance(float r1, float r2)
+{
+
+}
+float getPower(float current, float voltage)
+{
+
+}
