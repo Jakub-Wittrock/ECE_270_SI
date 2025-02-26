7127dace5bb7963fa8d57066441cafc1fa9cf5bb
Jakub-Wittrock <138022957+Jakub-Wittrock@users.noreply.github.com>
2025-02-25 21:53:40 -0500

    Create ofApp.cpp

---
+++ Graphing Calculator Project/Part 2 Starter Code/ofApp.cpp
@@ -0,0 +1,155 @@
+#include "ofApp.h"
+
+
+//--------------------------------------------------------------
+int ofApp::getUniformSamples(float* array, float min, float max, float step)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::printFloatArray(int n, float* array, char* label)
+{
+	
+}
+//--------------------------------------------------------------
+float ofApp::map(float value, float inMin, float outMin, float inMax, float outMax)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::mapArray(int n, float* array, float inMin, float outMin, float inMax, float outMax)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::getCosineSamples(int n, float* in, float* out)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::getSineSamples(int n, float* in, float* out)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::getLinearSamples(int n, float* in, float* out, float m, float b)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::getQuadraticSamples(int n, float* in, float* out, float a, float b, float c)
+{
+
+}
+//--------------------------------------------------------------
+void ofApp::getNormalDistributionSamples(int n, float* x, float* samples, float mu, float sigma)
+{
+
+}
+//--------------------------------------------------------------
+float ofApp::getSum(int n, float* array)
+{
+	
+}
+//--------------------------------------------------------------
+float ofApp::getAverage(int n, float* array)
+{
+
+}
+//--------------------------------------------------------------
+float ofApp::getStandardDeviation(int n, float* array)
+{
+
+}
+//--------------------------------------------------------------
+void ofApp::drawLine(int n, float* x, float* y, int lineWidth, int r, int g, int b)
+{
+	
+}
+//--------------------------------------------------------------
+void ofApp::drawGrid(int n, float* x, float* y,  int screenWidth, int screenHeight)
+{
+
+}
+//--------------------------------------------------------------
+float ofApp::getMin(int n, float* arr)
+{
+
+}
+//--------------------------------------------------------------
+float ofApp::getMax(int n, float* arr)
+{
+	
+}
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
